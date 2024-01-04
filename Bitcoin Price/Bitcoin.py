5076f9671af737ef7e6f597dd27d91f12ec5e7b1
reza.latifi <reza72rg@gmail.com>
2024-01-04 13:36:36 +0330

    Bitcoin_Price V "1.1"
    I use pyttsx3 module to tell bitcoin price on Windows

--- Bitcoin Price/Bitcoin.py
+++ Bitcoin Price/Bitcoin.py
@@ -1,8 +1,13 @@
 import requests
 import json
+import pyttsx3
 r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 
 # request part and response as r
 data = json.loads(r.text)
 price = data['bpi']['USD']['rate']
 print(f'Bitcoin Price is : {price}')
+price= int(float(price.replace(',','')))
+engine = pyttsx3.init()
+engine.say("Bitcoin price now is {}".format(price))
+engine.runAndWait()
